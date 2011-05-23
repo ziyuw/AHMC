fb68f4e4366e0ea40b8f5786b37e911ac241024b
Ziyu <steven@steven.(none)>
2011-05-23 00:08:14 -0700

    ...

--- src/config.py
+++ src/config.py
@@ -1,21 +1,5 @@
 import ConfigParser
 
-config = ConfigParser.RawConfigParser()
-
-config.add_section('Section1')
-config.set('Section1', 'command_path', '/ubc/cs/home/z/ziyuw/projects/AHMC/fbm.2004-11-10/')
-config.set('Section1', 'file_path', '/ubc/cs/home/z/ziyuw/projects/AHMC/log/')
-config.add_section('Section2')
-config.set('Section2', 'MADELON', '/ubc/cs/home/z/ziyuw/projects/AHMC/Data/MADELON/')
-config.add_section('Section3')
-config.set('Section3', 'int', '0')
-config.add_section('Section4')
-config.set('Section4', 'MADELON', '/ubc/cs/home/z/ziyuw/projects/AHMC/run_log/')
-# Writing our configuration file to 'path_config.cfg'
-with open('path_config.cfg', 'wb') as configfile:
-    config.write(configfile)
-
-
 class config:
     def __init__(self, config_file):
 	self.config_file = config_file
