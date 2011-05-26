b845b7a8bf79979253a5335a26ca6bb8921a0b98
Ziyu Wang <ziyucwang@gmail.com>
2011-05-25 23:11:39 -0700

    200 iterations

--- src/config_setup.py
+++ src/config_setup.py
@@ -2,13 +2,13 @@ import ConfigParser
 
 config = ConfigParser.RawConfigParser()
 
-home_path = '/home/steven/Documents/School/project/AHMC/'
+home_path = '/ubc/cs/home/z/ziyuw/projects/AHMC/'
 
 config.add_section('Section1')
 config.set('Section1', 'command_path', home_path+'fbm.2004-11-10/')
 config.set('Section1', 'file_path', home_path+'log/')
 config.add_section('Section2')
-config.set('Section2', 'MADELON', home_path+'Data/MADELON/MADELON/')
+config.set('Section2', 'MADELON', home_path+'Data/MADELON/')
 config.set('Section2', 'ROBOARM', home_path+'Data/RobotArm/')
 config.add_section('Section3')
 config.set('Section3', 'cur_counter', '0')
