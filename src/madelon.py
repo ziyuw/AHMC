8fe55fcbbe641d4d8adde2b3cc88af584c921cbe
Ziyu Wang <ziyucwang@gmail.com>
2011-05-30 11:17:37 -0700

    madelon

--- src/madelon.py
+++ src/madelon.py
@@ -105,12 +105,12 @@ fn = lambda x, item, epsilon: util.Gaussian_RBF_lambda(x, item, epsilon, lambdas
 opt = optimize(fn)
 
 if pure_bayes:
-    opt.bounds = [(0.3, 0.6), (50.0, 4000.0)]
-    opt.num_basis = 500
+    opt.bounds = [(0.3, 0.8), (50.0, 2000.0)]
+    opt.num_basis = 200
     opt.start_point = [0.4, 200.0]
     opt.maxeval = 100
-    opt.epsilons =  arange(13.5, 18.0, 0.5)
-    opt.bf_opt_steps = [0.02, 50.0]
+    opt.epsilons =  arange(0.5, 4.0, 0.5)
+    opt.bf_opt_steps = [0.05, 50.0]
 else:
     opt.bounds = [(10.0, 1010.0)]
     opt.num_basis = 500
