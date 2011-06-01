37217c6c081bcf10ae03acff81da691914f3ba18
Ziyu Wang <ziyucwang@gmail.com>
2011-05-31 21:28:54 -0700

    ensemble

--- src/ensemble_pred.py
+++ src/ensemble_pred.py
@@ -25,45 +25,52 @@ def parse_result(result):
 	l.append(int(line))
     return l
 
-conf = config('path_config.cfg')
 
-command_path = conf.get_command_path()
+def write_in_file(data_file_name, result_file_name):
 
-test_data_path = conf.get_data_path('DEXTER')+"test.data.sel"#+"combined_valid.data.sel"#'combined_valid.data.sel'
+    conf = config('path_config.cfg')
 
-cur_counter = '105'
-net_folder = conf.get_file_path("dexter", cur_counter)
-option = 'bm'
+    command_path = conf.get_command_path()
 
-num_folds = 10
+    test_data_path = conf.get_data_path('DEXTER') + data_file_name #+"test.data.sel"#+"combined_valid.data.sel"#'combined_valid.data.sel'
 
-ls = []
-for i in range(num_folds):
-    net_path = net_folder + '/dexter' + str(i) + '.net'
-    cmd = generate_netpred_command(11, net_path, option, command_path, test_data_path)
-    
-    process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
-    result = process.communicate()
-    
-    ls.append(parse_result(result))
+    cur_counter = '105'
+    net_folder = conf.get_file_path("dexter", cur_counter)
+    option = 'bm'
 
-result = []
-dim = len(ls[0])
-for i in range(dim):
-    s = sum([ls[j][i] for j in range(len(ls))])
-    if s > len(ls) - s:
-	result.append('1')
-    elif s < len(ls) - s:
-	result.append('-1')
-    else:
-	if random() > 0.5:
+    num_folds = 10
+
+    ls = []
+    for i in range(num_folds):
+	net_path = net_folder + '/dexter' + str(i) + '.net'
+	cmd = generate_netpred_command(11, net_path, option, command_path, test_data_path)
+	
+	process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
+	result = process.communicate()
+	
+	ls.append(parse_result(result))
+
+    result = []
+    dim = len(ls[0])
+    for i in range(dim):
+	s = sum([ls[j][i] for j in range(len(ls))])
+	if s > len(ls) - s:
 	    result.append('1')
-	else:
+	elif s < len(ls) - s:
 	    result.append('-1')
+	else:
+	    if random() > 0.5:
+		result.append('1')
+	    else:
+		result.append('-1')
 
-print len(result)
+    print len(result)
 
-f = open('./dexter_test.resu', 'w')
-for item in result:
-    f.write(item+"\n")
-f.close()
+    f = open(result_file_name, 'w')
+    for item in result:
+	f.write(item+"\n")
+    f.close()
+    
+write_in_file("test.data.sel", './dexter_test.resu')
+write_in_file("combined_valid.data.sel", './dexter_valid.resu')
+write_in_file("combined_train.data.sel", './dexter_train.resu')
