679f8eedb70e2c6135d38535bdb13e460089611d
Ziyu <steven@steven.(none)>
2011-09-24 15:17:11 -0700

    commit

--- src/parse_log_file.py
+++ src/parse_log_file.py
@@ -57,27 +57,11 @@ opt = optimize(fn)
 
 # Madelon
 #opt.bounds = [(0.3, 0.9), (50.0, 2000.0)]
-<<<<<<< HEAD
 #opt.num_basis = 200
 #opt.start_point = [0.4, 200.0]
 #opt.maxeval = 100
 #opt.epsilons =  arange(3.5, 10.0, 0.5)
 #opt.bf_opt_steps = [0.05, 50.0]
-=======
-#opt.num_basis = 200
-#opt.start_point = [0.4, 200.0]
-#opt.maxeval = 100
-#opt.epsilons =  arange(3.5, 10.0, 0.5)
-#opt.bf_opt_steps = [0.05, 50.0]
-
-#opt.bounds = [(0.2, 0.6), (50.0, 5000.0)]
-#opt.num_basis = 200
-#opt.start_point = [0.4, 200.0]
-#opt.maxeval = 100
-#opt.epsilons =  arange(0.5, 4.0, 0.5)
-#opt.bf_opt_steps = [0.05, 100.0]
-
->>>>>>> 34dcf318b245779036c975acb85e3e808d24c832
 
 # Robo
 opt.bounds = [(0.2, 0.6), (50.0, 5000.0)]
@@ -111,10 +95,6 @@ for line in f:
 	pt.append(float(line_spt[1]))
     
     if pt != None and reward != None:
-	# For robo arm
-	#reward = reward/1000.0 + 0.99
-	
-	
 	print pt, reward
 	x.append(pt[0])
 	y.append(pt[1])
@@ -133,13 +113,8 @@ if plot:
     
     contour = False
     
-<<<<<<< HEAD
     X = arange(0.2, 0.6, 0.05)
     Y = arange(50, 5000, 50.0)
-=======
-    X = arange(0.05, 0.6, 0.02)
-    Y = arange(50, 2000, 20.0)
->>>>>>> 34dcf318b245779036c975acb85e3e808d24c832
     X, Y = meshgrid(X, Y)
     Z = func(X, Y)
 
@@ -147,26 +122,16 @@ if plot:
 	plt.figure()
 	CS = plt.contour(X, Y, Z)
 	plt.clabel(CS, inline=1, fontsize=10)
-	plt.title('Surface learned by Bayesian ')
+	plt.title('Simplest default with labels')
     else:
 	fig = plt.figure()
 	ax = Axes3D(fig)
 	
-	scatter = ax.scatter(x, y, z, label='Samples drawn')
+	ax.scatter(x, y, z)
 	
 	surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
-		linewidth=0, antialiased=False, label='Surface learned by contexual bandits')
+		linewidth=0, antialiased=False)
 	fig.colorbar(surf, shrink=0.5, aspect=5)
-	
-	
-	ax.set_xlabel('Step size adjustment', fontsize=16)
-	ax.set_ylabel('No. of leapfrog steps', fontsize=16)
-	ax.set_zlabel('Reward', fontsize=16)
-	
-	#plt.rcParams['label.fontsize'] = 10
-	
-	# For robot arm
-	#ax.set_zlim3d(0.98, 1.00)
 
     plt.show()
     
